54293754bba43a2da861b4147defb01c3dc27139
Matt, Matthias <matthias.matt@tuwien.ac.at>
2024-07-07 17:42:46 +0200

    First fully working implementation

--- operator/operator.py
+++ operator/operator.py
@@ -1,9 +1,11 @@
+import json
 import kopf
 import kubernetes
 
 
 @kopf.on.create('configserver')
-def create_fn(meta, spec, memo: kopf.Memo, **kwargs):
+def create_fn(meta, spec, **kwargs):
+    # TODO: also read all Key-Value CRDs and create these for the correct ConfigMap
     client = kubernetes.client.api_client.ApiClient(configuration=kubernetes.config.load_kube_config())
     api = kubernetes.client.CoreV1Api(api_client=client)
     apps_api = kubernetes.client.AppsV1Api(api_client=client)
@@ -11,46 +13,47 @@ def create_fn(meta, spec, memo: kopf.Memo, **kwargs):
     name = meta["name"]
     namespace = meta["namespace"]
 
-    # TODO: remove memo if not used in handlers
-    memo.config_name = name
-    memo.namespace = namespace
-
-    configmap_manifest = {
-        "kind": "ConfigMap",
-        "apiVersion": "v1",
-        "metadata": {
-            "name": f"{name}-values",
-            "namespace": namespace
-        },
-        "data": {}
-    }
+    configmap_manifest = kubernetes.client.V1ConfigMap(api_version="v1",
+                                                       metadata={"name": f"{name}-values", "namespace": namespace},
+                                                       data={})
     api.create_namespaced_config_map(namespace, body=configmap_manifest)
+
+    service = kubernetes.client.V1Service(api_version="v1",
+                                          metadata={"name": name, "namespace": namespace},
+                                          spec={"type": "ClusterIP",
+                                                "ports": [
+                                                    {"protocol": "TCP", "port": spec["containerPort"],
+                                                     "targetPort": spec["containerPort"], "name": "http"}
+                                                ],
+                                                "selector": {"app": name}})
+    api.create_namespaced_service(namespace, body=service)
 
     server_deployment_manifest = {
         "kind": "Deployment",
         "apiVersion": "apps/v1",
         "metadata": {
-            "name": f"{name}-deployment",
+            "name": name,
             "namespace": namespace,
             "labels": {
-                "app": f"{name}-deployment"
+                "app": name
             }
         },
         "spec": {
             "replicas": 1,
             "selector": {
                 "matchLabels": {
-                    "app": f"{name}-deployment"
+                    "app": name
                 }
             },
             "template": {
-                "metadata": {},
-                "labels": {
-                    "app": f"{name}-deployment"
+                "metadata": {
+                    "labels": {
+                        "app": name
+                    }
                 },
                 "spec": {
                     "containers": [{
-                        "name": f"{name}",
+                        "name": name,
                         "image": spec["image"],
                         "imagePullPolicy": spec["imagePullPolicy"],
                         "ports": [{
@@ -58,10 +61,13 @@ def create_fn(meta, spec, memo: kopf.Memo, **kwargs):
                             "containerPort": spec["containerPort"],
                             "protocol": "TCP",
                         }],
+                        "env": [
+                            {"name": "CONFIG_SERVER_DIR", "value": spec["configMountPath"]},
+                            {"name": "CONFIG_SERVER_PORT", "value": str(spec["containerPort"])},
+                        ],
                         "volumeMounts": [{
                             "name": "config",
                             "mountPath": spec["configMountPath"],
-                            "subPath": spec["configSubPath"]
                         }]
                     }],
                     "volumes": [{
@@ -78,31 +84,65 @@ def create_fn(meta, spec, memo: kopf.Memo, **kwargs):
     apps_api.create_namespaced_deployment(namespace, body=server_deployment_manifest)
 
 
-def _get_config_map(config_name, namespace) -> tuple[kubernetes.client.V1ConfigMap, kubernetes.client.CoreV1Api]:
-    # TODO: add error handling
+@kopf.on.delete('configserver')
+def delete_fn(meta, spec, **kwargs):
     client = kubernetes.client.api_client.ApiClient(configuration=kubernetes.config.load_kube_config())
     api = kubernetes.client.CoreV1Api(api_client=client)
-    config_map = api.read_namespaced_config_map(name=f"{config_name}-values", namespace=namespace)
-    return config_map, api
+    apps_api = kubernetes.client.AppsV1Api(api_client=client)
+
+    name = meta["name"]
+    namespace = meta["namespace"]
+
+    try:
+        apps_api.delete_namespaced_deployment(name, namespace)
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            pass
+
+    try:
+        api.delete_namespaced_service(name, namespace)
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            pass
+
+    try:
+        api.delete_namespaced_config_map(f"{name}-values", namespace)
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            pass
+
+
+def _get_config_map(config_name, namespace) -> tuple[kubernetes.client.V1ConfigMap, kubernetes.client.CoreV1Api]:
+    client = kubernetes.client.api_client.ApiClient(configuration=kubernetes.config.load_kube_config())
+    api = kubernetes.client.CoreV1Api(api_client=client)
+    try:
+        config_map = api.read_namespaced_config_map(name=f"{config_name}-values", namespace=namespace)
+        if config_map.data is None:
+            config_map.data = {}
+        return config_map, api
+    except kubernetes.client.exceptions.ApiException as e:
+        if e.status == 404:
+            kopf.PermanentError(f"ConfigMap {config_name}-values not found! First create a valid ConfigServer.")
 
 
 @kopf.on.create('keyvaluepair')
 @kopf.on.update('keyvaluepair')
 def create_config_fn(meta, spec, **kwargs):
-    config_name = meta["config"]
+    # TODO: split update and create handlers to check if key already exists in create step
+    config_name = spec["config"]
     namespace = meta["namespace"]
     config_map, api = _get_config_map(config_name, namespace)
 
-    # TODO: maybe assert that spec["value"] is valid yaml which can be parsed in server
-    config_map.data[spec["key"]] = spec["value"]  # TODO: in-place update?
+    # config_map.data has to be of type dict[str, str] so encode values as json string
+    config_map.data[spec["key"]] = json.dumps(spec["value"])
     api.patch_namespaced_config_map(name=f"{config_name}-values", namespace=namespace, body=config_map)
 
 
 @kopf.on.delete('keyvaluepair')
-def detete_config_fn(meta, spec, **kwargs):
-    config_name = meta["config"]
+def delete_config_fn(meta, spec, **kwargs):
+    config_name = spec["config"]
     namespace = meta["namespace"]
     config_map, api = _get_config_map(config_name, namespace)
 
-    config_map.data.pop(spec["key"])  # TODO: in-place update?
+    config_map.data.pop(spec["key"], None)
     api.patch_namespaced_config_map(name=f"{config_name}-values", namespace=namespace, body=config_map)
--- server/server.py
+++ server/server.py
@@ -1,19 +1,25 @@
 import argparse
 import asyncio
 import json
+import logging
+import os
+import sys
 from pathlib import Path
-import yaml
-from yaml import YAMLError
 import tornado
+
+log = logging.getLogger(__name__)
 
 
 def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog='Config Server',
                                      description="Hosts REST API that can be used to access keys of a YAML config file")
-
-    parser.add_argument('-f', '--config-values', default='/var/lib/config-server/values.yaml', type=str,
-                        help='the key/value pairs of the config map')
-    parser.add_argument('-p', '--port', default=80, type=int,
+    parser.add_argument('-f', '--config-dir',
+                        default=os.environ.get('CONFIG_SERVER_DIR', '/var/lib/config-server'),
+                        type=str,
+                        help='directory where the key/value pairs of the config map are mounted')
+    parser.add_argument('-p', '--port',
+                        default=int(os.environ.get('CONFIG_SERVER_PORT', '80')),
+                        type=int,
                         help='port to listen on')
     return parser
 
@@ -21,28 +27,43 @@ def get_parser() -> argparse.ArgumentParser:
 class KeyValueHandler(tornado.web.RequestHandler):
     def get(self, key: str):
         try:
-            with open(self.settings['config_values'], 'r') as f:
-                config_values = yaml.safe_load(f)
-            value: dict = config_values[key]
-            self.write(json.dumps(value))
-        except YAMLError:
-            raise tornado.web.HTTPError(500, reason="Failed to load config values")
-        except KeyError:
-            raise tornado.web.HTTPError(404, reason="Key not found")
+            path = self.settings['config_values'] / key
+            with open(path, 'r') as f:
+                value = json.load(f)
+            self.write(value)
+        except json.decoder.JSONDecodeError:
+            raise tornado.web.HTTPError(400, reason=f"Failed to load values for key '{key}': not valid JSON")
+        except FileNotFoundError:
+            raise tornado.web.HTTPError(404, reason=f"Key '{key}' not found")
 
 
 def make_app(config_values: Path) -> tornado.web.Application:
     return tornado.web.Application(
         handlers=[
-            (r"/(?P<key>[\d\w]+)\/?", KeyValueHandler),
+            (r"/key/(?P<key>[\d\w]+)\/?", KeyValueHandler),
         ],
         # settings:
         config_values=config_values,
     )
 
 
+def init_logs():
+    root = logging.getLogger()
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    root.addHandler(handler)
+
+    log.setLevel(logging.INFO)
+    logging.getLogger("tornado.access").setLevel(logging.INFO)
+    logging.getLogger("tornado.application").setLevel(logging.INFO)
+    logging.getLogger("tornado.general").setLevel(logging.INFO)
+
+
 async def start(config_values: Path, port: int):
+    init_logs()
+    log.info(f"Starting server - directory: {config_values}")
     app = make_app(config_values)
+    log.info(f"Serving on port {port}")
     app.listen(port)
     shutdown_event = asyncio.Event()
     await shutdown_event.wait()
@@ -51,7 +72,7 @@ async def start(config_values: Path, port: int):
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    config_values = Path(args.config_values)
+    config_values = Path(args.config_dir)
     assert config_values.exists()
     port = args.port
 
